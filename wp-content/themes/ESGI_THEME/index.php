2ca5ad351010ba0a9a2d6f46e56d51694756d97c
Ouail Amarir <ouailamarir@gmail.com>
2025-07-17 14:42:36 +0200

    Modification du fichier index.php pour intégrer l'en-tête et le pied de page, ajout d'une structure HTML avec le titre et le contenu de la page.

--- wp-content/themes/ESGI_THEME/index.php
+++ wp-content/themes/ESGI_THEME/index.php
@@ -1,8 +1,10 @@
 <?php
-
-/*
-Template Name: ESGI Thème
-*/
-
+get_header();
 ?>
-                     
+<main>
+  <h1><?php the_title(); ?></h1>
+  <div class="page-content">
+    <?php the_content(); ?>
+  </div>
+</main>
+<?php get_footer(); ?>
