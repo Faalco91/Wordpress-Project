8439709bfc2994e8249c598e2dd03bcc3a541b15
Ouail Amarir <ouailamarir@gmail.com>
2025-07-17 14:43:01 +0200

    Modification de front-page.php pour intégrer des champs personnalisés avec ACF, remplaçant le contenu statique par des données dynamiques pour les sections "Héros", "À propos", "Services" et "Partenaires".

--- wp-content/themes/ESGI_THEME/front-page.php
+++ wp-content/themes/ESGI_THEME/front-page.php
@@ -9,38 +9,38 @@ get_header();
   <!-- HERO -->
   <section class="intro-container">
     <div class="page-main-title">
-      <h1 class="main-title">A really professional structure for all your events!</h1>
+      <h1 class="main-title"><?php the_field('intro-title'); ?></h1>
     </div>
     <div class="img-container">
-      <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/image1_home.png" alt="Event Photo">
+      <img src="<?php the_field('intro-img'); ?>" alt="">
     </div>
   </section>
 
   <!-- ABOUT US -->
   <section class="about-us">
-
     <div class="information-1">
-      <h2>About Us</h2>
-      <p>Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution.</p>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <h2><?php the_field('section-title-1'); ?></h2>
+      <p><?php the_field('section-description-1'); ?></p>
+    <?php endwhile; endif; ?>
     </div>
-
     <!-- BLOCK WHO/VISION/MISSION -->
     <div class="about-us-content">
       <div class="about-us-img">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_1_about.png" alt="">
+        <img src="<?php the_field('section-1-img-1'); ?>" alt="">
       </div>
       <div class="information information-2">
         <div class="information-2-content">
-          <h3>Who are we?</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu convallis elit, at convallis magna.</p>
+          <h3><?php the_field('info-h3-1') ?></h3>
+          <p><?php the_field('info-description-1') ?></p>
         </div>
         <div class="information-2-content">
-          <h3>Our vision</h3>
-          <p> Nullam faucibus interdum massa. Duis eget leo mattis, pulvinar nisi et, consequat lectus. Suspendisse commodo magna orci, id luctus risus porta pharetra. Fusce vehicula aliquet urna non ultricies.</p>
+          <h3><?php the_field('info-h3-2') ?></h3>
+          <p><?php the_field('info-description-2') ?></p>
         </div>
         <div class="information-2-content">
-          <h3>Our mission</h3>
-          <p>Vivamus et viverra neque, ut pharetra ipsum. Aliquam eget consequat libero, quis cursus tortor. Aliquam suscipit eros sit amet velit malesuada dapibus. Fusce in vehicula tellus. Donec quis lorem ut magna tincidunt egestas. </p>
+          <h3><?php the_field('info-h3-3') ?></h3>
+          <p><?php the_field('info-description-3') ?></p>
         </div>
       </div>
     </div>
@@ -48,36 +48,35 @@ get_header();
 
   <!-- SERVICES -->
   <section class="services">
-    <h2>Our Services</h2>
+    <h2><?php the_field('section-title-2') ?></h2>
     <div class="services-list">
       <div class="service-item service-1">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_1_services.png" alt="">
+        <img src="<?php the_field('section-2-img-1'); ?>" alt="">
       </div>
       <div class="service-item service-2">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_2_services.png" alt="">
+        <img src="<?php the_field('section-2-img-2'); ?>" alt="">
       </div>
       <div class="service-item service-3">
-        <p>Private Parties
+        <p><?php the_field('text-section-2') ?>
           <span class="service-bar"></span>
         </p>
       </div>
       <div class="service-item service-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_3_services.png" alt="">
+        <img src="<?php the_field('section-2-img-3'); ?>" alt="">
       </div>
     </div>
   </section>
 
   <!-- PARTNERS -->
   <section class="partners">
-    <h2>Our Partners</h2>
+    <h2><?php the_field('section-title-3') ?></h2>
     <div class="partners-logos">
-      <img class="partner-logo-1" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_1_partners.png" alt="">
-      <img class="partner-logo-3" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_3_partners.png" alt="">
-      <img class="partner-logo-4" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_4_partners.png" alt="">
-      <img class="partner-logo-5" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_5_partners.png" alt="">
-      <img class="partner-logo-6" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_6_partners.png" alt="">
-
-      <!-- ... -->
+      <img class="partner-logo-1" src="<?php the_field('section-3-img-1'); ?>" alt="">
+      <img class="partner-logo-3" src="<?php the_field('section-3-img-2'); ?>" alt="">
+      <img class="partner-logo-4" src="<?php the_field('section-3-img-3'); ?>" alt="">
+      <img class="partner-logo-5" src="<?php the_field('section-3-img-4'); ?>" alt="">
+      <img class="partner-logo-5"  src="<?php the_field('section-3-img-5'); ?>" alt="">
+      <img class="partner-logo-6"  src="<?php the_field('section-3-img-6'); ?>" alt="">
     </div>
   </section>
 </main>
