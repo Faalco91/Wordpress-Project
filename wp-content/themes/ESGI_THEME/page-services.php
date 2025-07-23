d7a7a5970f0eea0b08f41e29c9d2e90cc1681512
Yanis Pires <ypires@fiveforty.fr>
2025-07-23 14:22:49 +0200

    Revert "Ajout de la configuration de phpMyAdmin dans docker-compose et mise à jour des chemins d'images dans les fichiers de thème pour une meilleure gestion des ressources." Revert: Annulation des modifications d'images et templates This reverts commit 86ac9166a848635bde89612e8ae18733f6b7446d.

--- wp-content/themes/ESGI_THEME/page-services.php
+++ wp-content/themes/ESGI_THEME/page-services.php
@@ -6,37 +6,33 @@ get_header();
 ?>
 
 <main>
-  <!-- SERVICES SECTION -->
+  <!-- SERVICES -->
   <section id="services-page" class="services">
-    <h2>Our Services.</h2>
+    <h2><?php the_field('section-title-1') ?></h2>
     <div class="services-list">
       <div class="service-item service-1">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_1_services.png" alt="Service 1"/>
+        <img src="<?php the_field('section-1-img-1'); ?>"/>
       </div>
       <div class="service-item service-2">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_2_services.png" alt="Service 2"/>
+        <img src="<?php the_field('section-1-img-2'); ?>"/>
       </div>
       <div class="service-item service-3">
-        <p>Private Parties
+        <p><?php the_field('text-section-1') ?>
           <span class="service-bar"></span>
         </p>
       </div>
       <div class="service-item service-4">
-        <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_3_services.png" alt="Service 3"/>
+        <img src="<?php the_field('section-1-img-3'); ?>"/>
       </div>
     </div>
 
-    <!-- PRIVATE PARTIES SECTION -->
     <div id="private-parties" class="information-1">
         <div class="information-1-content">
-            <h2>Corp. Parties</h2>
-            <p>Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution.</p>
+            <h2><?php the_field('section-1-h2'); ?></h2>
+            <p><?php the_field('section-1-description-1'); ?></p>
         </div>
     </div>
-    
-    <div class="full-width-image-container">
-        <img class="full-width-image" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/img_1_corpParties.png" alt="Private Parties"/>
-    </div>
+    <img src="<?php the_field("section-1-img-4") ?>"/>
 
   </section>
 
